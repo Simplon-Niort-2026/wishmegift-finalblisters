4135cfcd2c49c1c4d97d8a2044bae526c6f4d083
BOULET Pierre <pierre.boulet@maif.fr>
2026-04-27 16:42:30 +0200

    feat: add @Nonnull on several fields + add created & update fields

--- src/main/java/co/simplon/wishmegift/entity/Gift.java
+++ src/main/java/co/simplon/wishmegift/entity/Gift.java
@@ -1,7 +1,11 @@
 package co.simplon.wishmegift.entity;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -12,19 +16,23 @@ public class Gift {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "name", length = 50)
+    @Column(name = "name", length = 50, nullable = false)
+    @Nonnull
     private String name;
 
     @Column(name = "description", columnDefinition = "TEXT", nullable = true)
     private String description;
 
-    @Column(name = "link", length = 255)
+    @Column(name = "link", length = 255,nullable = false)
+    @Nonnull
     private String link;
 
-    @Column(name = "desire_level")
+    @Column(name = "desire_level", nullable = false)
+    @Nonnull
     private Integer desire_level;
 
-    @Column(name = "price", columnDefinition = "DECIMAL")
+    @Column(name = "price", columnDefinition = "DECIMAL", nullable = false)
+    @Nonnull
     private Float price;
 
     @Column(name = "reserved", columnDefinition = "false")
@@ -33,6 +41,16 @@ public class Gift {
     @Column(name = "reservedBy", nullable = true)
     private UUID reservedBy;
 
+    @Column
+    @Nonnull
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column
+    @Nonnull
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
     public Gift() {
     }
 
@@ -40,14 +58,11 @@ public class Gift {
         return id;
     }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
+    @Nonnull
     public void setName(String name) {
         this.name = name;
     }
@@ -64,6 +79,7 @@ public class Gift {
         return link;
     }
 
+    @Nonnull
     public void setLink(String link) {
         this.link = link;
     }
@@ -72,6 +88,7 @@ public class Gift {
         return desire_level;
     }
 
+    @Nonnull
     public void setDesire_level(Integer desire_level) {
         if(desire_level < 0 || desire_level > 5) {
             throw new IllegalArgumentException("Desire level must be between 1 and 5");
@@ -79,6 +96,7 @@ public class Gift {
         this.desire_level = desire_level;
     }
 
+    @Nonnull
     public Float getPrice() {
         return price;
     }
@@ -101,5 +119,15 @@ public class Gift {
 
     public void setReservedBy(UUID reservedBy) {
         this.reservedBy = reservedBy;
+    }
+
+    @Nonnull
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Nonnull
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
     }
 }
