d250b301de71f4bad1d42277ccf6fa3508f0656f
BOULET Pierre <pierre.boulet@maif.fr>
2026-04-27 15:33:45 +0200

    feat: edited pom.xml + application.properties for db / created gift entity / add customer interview

---
+++ src/CR_Entretien-client
@@ -0,0 +1,26 @@
+# Compte-rendu d'entretien
+
+## Pitch
+En raison de l'_enshitification_ de Pinterest avec l'IA notamment, il souhaite proposer une alternative et voir si le projet est viable ou pas.
+
+## Clientèle cible
+Le client type est plutôt envisagé comme ayant des moyens de classe moyenne. Il est possible d'anticiper des montants plus importants pour convenir à une clientèle avec des moyens plus conséquents.
+
+## Précisions sur certains aspects
+- la date de l'évènement sert aussi de date butoir. C'est-à-dire qu'un utilisateur ne peut plus faire de réservations une fois la date passée,
+- une description peut être vide,
+- un lien est une URL qui peut autant pointer vers une boutique en ligne ou vers un site descriptif,
+- le cadeau a un "statut" car il doit être indisponible s'il a été réservé par un autre utilisateur,
+- un visiteur peut consulter une liste mais il ne peut pas agir dessus (il ne peut pas réserver par exemple),
+- un utilisateur peut avoir plusieurs listes,
+- un utilisateur peut avoir plusieurs listes sur le même thème,
+- nous ne sommes pas concerné pas le front-end.
+
+## Attendu du POC
+- pouvoir partager des listes entre des utilisateurs factices pour mettre à l'épreuve,
+- fournir une API à consommer pour le front-end.
+
+## Élargissement envisageable du POC
+- créer des listes pour une tierce personne,
+- gestion du mail d'invitation à consulter.
+
---
+++ src/main/java/co/simplon/wishmegift/entity/Gift.java
@@ -0,0 +1,101 @@
+package co.simplon.wishmegift.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class Gift {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT", nullable = true)
+    private String description;
+
+    @Column(name = "link", length = 255)
+    private String link;
+
+    @Column(name = "desire_level")
+    private Integer desire_level;
+
+    @Column(name = "price", columnDefinition = "DECIMAL")
+    private Float price;
+
+    @Column(name = "reserved", columnDefinition = "false")
+    private Boolean reserved = false;
+
+    @Column(name = "reservedBy", nullable = true)
+    private UUID reservedBy;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Integer getDesire_level() {
+        return desire_level;
+    }
+
+    public void setDesire_level(Integer desire_level) {
+        if(desire_level < 0 || desire_level > 6) {
+            throw new IllegalArgumentException("Desire level must be between 1 and 5");
+        }
+        this.desire_level = desire_level;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Boolean getReserved() {
+        return reserved;
+    }
+
+    public void setReserved(Boolean reserved) {
+        this.reserved = reserved;
+    }
+
+    public UUID getReservedBy() {
+        return reservedBy;
+    }
+
+    public void setReservedBy(UUID reservedBy) {
+        this.reservedBy = reservedBy;
+    }
+}
