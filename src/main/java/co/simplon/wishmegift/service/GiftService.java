93069b0445ad588b6d1848bd880eac9bff544fd8
BOULET Pierre <pierre.boulet@maif.fr>
2026-04-28 16:12:25 +0200

    feat: added addGift method in GiftService

--- src/main/java/co/simplon/wishmegift/service/GiftService.java
+++ src/main/java/co/simplon/wishmegift/service/GiftService.java
@@ -1,6 +1,20 @@
 package co.simplon.wishmegift.service;
 
+import co.simplon.wishmegift.entity.GiftEntity;
+import co.simplon.wishmegift.repository.GiftRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GiftService {
 
+    @Autowired
+    private GiftRepository giftRepository;
+
+    public GiftEntity addGift(GiftEntity gift) {
+        return giftRepository.save(gift);
+    }
 
 }
