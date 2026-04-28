bfe5ff31337467d322931022c5f609bf03d6050e
Pierre <66121468+Zatigan@users.noreply.github.com>
2026-04-28 13:36:20 +0200

    Feature/create entity gift (#22)
    * feat: edited pom.xml + application.properties for db / created gift entity / add customer interview
    
    * feat: changed desire_level condition to be adequat with expectations
    
    * fix: customer interview moved to repo root
    
    * feat: addition of empty constructor for gift
    
    * feat: added table name definition
    
    * feat: add @Nonnull on several fields + add created & update fields
    
    * feat: created folder for documentation and modified entity table name
    
    * feat: commented reserved_by until access to user table
    
    ---------
    
    Co-authored-by: BOULET Pierre <pierre.boulet@maif.fr>

---
+++ src/main/java/co/simplon/wishmegift/entity/Gift.java
@@ -0,0 +1,134 @@
+package co.simplon.wishmegift.entity;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "gift")
+public class Gift {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    @Nonnull
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT", nullable = true)
+    private String description;
+
+    @Column(name = "link", length = 255,nullable = false)
+    @Nonnull
+    private String link;
+
+    @Column(name = "desire_level", nullable = false)
+    @Nonnull
+    private Integer desire_level;
+
+    @Column(name = "price", columnDefinition = "DECIMAL", nullable = false)
+    @Nonnull
+    private Float price;
+
+    @Column(name = "reserved", columnDefinition = "false")
+    private Boolean reserved = false;
+
+//    Ligne à décomenter lorsque la table User sera accessible
+//    @Column(name = "reserved_by", nullable = true)
+//    private UUID reservedBy;
+
+    @Column
+    @Nonnull
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column
+    @Nonnull
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Gift() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Nonnull
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    @Nonnull
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Integer getDesire_level() {
+        return desire_level;
+    }
+
+    @Nonnull
+    public void setDesire_level(Integer desire_level) {
+        if(desire_level < 0 || desire_level > 5) {
+            throw new IllegalArgumentException("Desire level must be between 1 and 5");
+        }
+        this.desire_level = desire_level;
+    }
+
+    @Nonnull
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Boolean getReserved() {
+        return reserved;
+    }
+
+    public void setReserved(Boolean reserved) {
+        this.reserved = reserved;
+    }
+
+    public UUID getReservedBy() {
+        return reservedBy;
+    }
+
+    public void setReservedBy(UUID reservedBy) {
+        this.reservedBy = reservedBy;
+    }
+
+    @Nonnull
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Nonnull
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
