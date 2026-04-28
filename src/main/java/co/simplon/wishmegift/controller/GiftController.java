d2326dfc56e7bb7a899e3622f69914e0d51ac018
BOULET Pierre <pierre.boulet@maif.fr>
2026-04-28 16:27:14 +0200

    feat: added post route in GiftController to add one gift

--- src/main/java/co/simplon/wishmegift/controller/GiftController.java
+++ src/main/java/co/simplon/wishmegift/controller/GiftController.java
@@ -1,4 +1,23 @@
 package co.simplon.wishmegift.controller;
 
+import co.simplon.wishmegift.entity.GiftEntity;
+import co.simplon.wishmegift.service.GiftService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class GiftController {
+
+    @Autowired
+    private GiftService giftService;
+
+    @PostMapping("/gifts/list/{list_id}/users/{users_id}")
+    public ResponseEntity<GiftEntity> addOneGift(@RequestBody GiftEntity gift) {
+        GiftEntity giftSaved = this.giftService.addGift(gift);
+        return new ResponseEntity<>(giftSaved, HttpStatus.CREATED);
+    }
 }
--- src/main/java/co/simplon/wishmegift/repository/GiftRepository.java
+++ src/main/java/co/simplon/wishmegift/repository/GiftRepository.java
@@ -1,4 +1,9 @@
 package co.simplon.wishmegift.repository;
 
-public interface GiftRepository {
+import co.simplon.wishmegift.entity.GiftEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GiftRepository extends JpaRepository<GiftEntity, Long> {
 }
