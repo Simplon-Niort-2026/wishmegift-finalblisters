a1ea145a27dbe42af1f5639ff54e70a7d1b13258
MICHEL Christophe <christophe.michel@externe.maif.fr>
2026-04-30 11:31:08 +0200

    feature: add possibilitity to update user

--- src/main/java/co/simplon/wishmegift/service/UserService.java
+++ src/main/java/co/simplon/wishmegift/service/UserService.java
@@ -7,6 +7,9 @@ import co.simplon.wishmegift.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class UserService {
 
@@ -39,5 +42,43 @@ public class UserService {
         }
 
         return userFound;
+    }
+
+    public UserEntity update(UserEntity userReceived) {
+        UUID userId = userReceived.getId();
+        Optional<UserEntity> userFound = userRepository.findById(userId);
+
+        if (userFound.isEmpty()) {
+            throw new BadRequestException("Id invalide.");
+        }
+
+        UserEntity userToUpdate = userFound.get();
+        String userToUpdateEmail = userToUpdate.getEmail();
+        String userReceivedEmail = userReceived.getEmail();
+        String userToUpdatePassword = userToUpdate.getPassword();
+        String userReceivedPassword = userReceived.getPassword();
+
+
+        if (userReceivedEmail != null && !userToUpdateEmail.equals(userReceivedEmail)) {
+            if (!RegexService.isValidEmail(userReceivedEmail)) {
+                throw  new BadRequestException("Email invalide.");
+            }
+
+            UserEntity userAlreadyExist =  userRepository.findByEmail(userReceivedEmail);
+            if (userAlreadyExist != null) {
+                throw  new BadRequestException("Email déjà utilisée.");
+            }
+            userToUpdate.setEmail(userReceivedEmail);
+        }
+        System.out.println("mot de passe reçu : "+ userReceivedPassword);
+        if (userReceivedPassword != null && !HasherService.isGoodPassword(userReceivedPassword, userToUpdatePassword)) {
+            if(!RegexService.isValidPassword(userReceivedPassword)) {
+                throw  new BadRequestException("Password invalide.");
+            }
+
+            userToUpdate.setPassword(HasherService.hash(userReceivedPassword));
+        }
+
+        return userRepository.save(userToUpdate);
     }
 }
