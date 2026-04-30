f6c28a839c6c1f1a5c3b7522f3f77dd0ce9549c0
BOULET Pierre <pierre.boulet@maif.fr>
2026-04-30 13:42:06 +0200

    fix: removed dead lines code

--- src/main/java/co/simplon/wishmegift/controller/GiftController.java
+++ src/main/java/co/simplon/wishmegift/controller/GiftController.java
@@ -1,19 +1,15 @@
 package co.simplon.wishmegift.controller;
 
 import co.simplon.wishmegift.entity.GiftEntity;
-import co.simplon.wishmegift.entity.WishListEntity;
-import co.simplon.wishmegift.errorsHandler.WishListNotFoundException;
 import co.simplon.wishmegift.repository.GiftRepository;
 import co.simplon.wishmegift.service.GiftService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RequestMapping("/gifts")
