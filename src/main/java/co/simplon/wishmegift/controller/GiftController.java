0f32901c0bf87618d4c7033fdc3bdbe26edfcce5
BOULET Pierre <pierre.boulet@maif.fr>
2026-04-30 13:35:55 +0200

    feat: delete one gift from its ID ok

--- src/main/java/co/simplon/wishmegift/controller/GiftController.java
+++ src/main/java/co/simplon/wishmegift/controller/GiftController.java
@@ -1,6 +1,8 @@
 package co.simplon.wishmegift.controller;
 
 import co.simplon.wishmegift.entity.GiftEntity;
+import co.simplon.wishmegift.entity.WishListEntity;
+import co.simplon.wishmegift.errorsHandler.WishListNotFoundException;
 import co.simplon.wishmegift.repository.GiftRepository;
 import co.simplon.wishmegift.service.GiftService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RequestMapping("/gifts")
 @RestController
@@ -36,5 +40,14 @@ public class GiftController {
     public ResponseEntity<GiftEntity> addOneGift(@RequestBody GiftEntity gift) {
         GiftEntity giftSaved = this.giftService.createGift(gift);
         return new ResponseEntity<>(giftSaved, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{gift_id}")
+    public void deleteGift(@PathVariable("gift_id") final UUID id) {
+        if(giftRepository.existsById(id)) {
+            giftRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Suppression impossible");
+        }
     }
 }
